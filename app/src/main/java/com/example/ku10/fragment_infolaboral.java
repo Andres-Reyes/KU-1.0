2fbd19b50b36e9161805fe7c08ca62a55e4badee
warriorcris25 <104987643+warriorcris25@users.noreply.github.com>
2022-05-25 08:35:18 -0500

    get commit

--- app/src/main/java/com/example/ku10/fragment_infolaboral.java
+++ app/src/main/java/com/example/ku10/fragment_infolaboral.java
@@ -7,14 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ofertaslaborales#newInstance} factory method to
+ * Use the {@link fragment_infolaboral#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ofertaslaborales extends Fragment {
+public class fragment_infolaboral extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,7 +24,7 @@ public class ofertaslaborales extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ofertaslaborales() {
+    public fragment_infolaboral() {
         // Required empty public constructor
     }
 
@@ -35,11 +34,11 @@ public class ofertaslaborales extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ofertaslaborales.
+     * @return A new instance of fragment fragment_infolaboral.
      */
     // TODO: Rename and change types and number of parameters
-    public static ofertaslaborales newInstance(String param1, String param2) {
-        ofertaslaborales fragment = new ofertaslaborales();
+    public static fragment_infolaboral newInstance(String param1, String param2) {
+        fragment_infolaboral fragment = new fragment_infolaboral();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,17 +58,7 @@ public class ofertaslaborales extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_laboral,
-                container, false);
-        Button button = (Button) view.findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                // do something
-            }
-        });
-        return view;
-    }}
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_infolaboral, container, false);
+    }
+}
