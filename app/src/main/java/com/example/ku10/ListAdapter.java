aaec9b9ac6edc4e0792c2c844866f2b8501cb137
Valen21ina <105886108+Valen21ina@users.noreply.github.com>
2022-05-26 07:55:46 -0500

    Get commit

---
+++ app/src/main/java/com/example/ku10/ListAdapter.java
@@ -0,0 +1,66 @@
+package com.example.ku10;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>{
+
+    private List<ListElement> mData;
+    private LayoutInflater mInflater;
+    private Context context;
+
+    public ListAdapter(List<ListElement> itemList, Context context){
+        this.mInflater = LayoutInflater.from(context);
+        this.context = context;
+        this.mData = itemList;
+    }
+
+    @Override
+    public int getItemCount() {return mData.size(); }
+
+    @Override
+    public ListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = mInflater.inflate(R.layout.list_element, null);
+        return new ListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ListAdapter.ViewHolder holder, final int position){
+        holder.bindData(mData.get(position));
+    }
+
+    public void setItems(List<ListElement> items){mData =items; }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView iconImageView;
+        TextView name, city, status, hora;
+
+        ViewHolder(View itemView){
+            super(itemView);
+            iconImageView = itemView.findViewById(R.id.iconImageView);
+            name = itemView.findViewById(R.id.nameTextView);
+            city = itemView.findViewById(R.id.cityTextView);
+            status = itemView.findViewById(R.id.statusTextView);
+            hora = itemView.findViewById(R.id.horaTextView);
+        }
+
+        void bindData(final ListElement item){
+            iconImageView.setColorFilter(Color.parseColor(item.getColor()), PorterDuff.Mode.SRC_IN);
+            name.setText(item.getName());
+            city.setText(item.getCity());
+            status.setText(item.getStatus());
+            hora.setText(item.getHora());
+        }
+
+    }
+}
