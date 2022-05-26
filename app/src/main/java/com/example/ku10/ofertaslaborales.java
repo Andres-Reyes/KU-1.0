6a260b834baa1f1752c2f50ce88d000b568b0269
warriorcris25 <104987643+warriorcris25@users.noreply.github.com>
2022-05-26 07:12:53 -0500

    get commit

--- app/src/main/java/com/example/ku10/ofertaslaborales.java
+++ app/src/main/java/com/example/ku10/ofertaslaborales.java
@@ -1,5 +1,6 @@
 package com.example.ku10;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,68 +9,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ofertaslaborales#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ofertaslaborales extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class ofertaslaborales extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ofertaslaborales() {
-        // Required empty public constructor
+    public void PlaceholderFragment() {
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ofertaslaborales.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ofertaslaborales newInstance(String param1, String param2) {
-        ofertaslaborales fragment = new ofertaslaborales();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_laboral, container, false);
 
-        View view = inflater.inflate(R.layout.fragment_laboral,
-                container, false);
-        Button button = (Button) view.findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                // do something
-            }
-        });
-        return view;
-    }}
+        Button btn = (Button) rootView.findViewById(R.id.button1);
+        btn.setOnClickListener(this);
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent (v.getContext(), ofertaslaborales.class);
+        startActivityForResult(intent, 0);
+        switch (v.getId()) {
+
+            case R.id.button1:
+                Toast.makeText(getActivity(), "Button clicked", Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+}
